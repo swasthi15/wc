27cb999fbfa49cab36a96bb97c55798c4c93d3f9
Swasthi B S <gaurav@drogon.dx.am>
2018-07-24 18:54:34 +0000

    second commit

--- wc.py
+++ wc.py
@@ -1,1 +1,11 @@
-print('hello')
+import requests
+from bs4 import BeautifulSoup
+
+def wc():
+        word = input("enter\n")
+        url= 'https://www.youtube.com/results?search_query=' +word
+        source_code=requests.get(url)
+        plain_text=source_code.text
+        soup=BeautifulSoup(plain_text)
+        print(soup)
+wc()
