695c2f865dd89f8e66722200f7ca79684b7b30c5
Swasthi B S <gaurav@drogon.dx.am>
2018-07-24 19:35:34 +0000

    third commit

--- wc.py
+++ wc.py
@@ -2,10 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def wc():
-        word = input("enter\n")
+        word = raw_input("enter\n")
         url= 'https://www.youtube.com/results?search_query=' +word
         source_code=requests.get(url)
         plain_text=source_code.text
-        soup=BeautifulSoup(plain_text)
-        print(soup)
+        soup=BeautifulSoup(plain_text,"html5lib")
+        for link in soup.findAll('a',{'class':' yt-uix-sessionlink spf-link '}):
+            href="https://youtube.com" +link.get('href')
+            title=link.string
+            print(href)
+            print(title)
 wc()
