a598dfce64139b49e97807c817bc86e02681a3d3
Swasthi B S <gaurav@drogon.dx.am>
2018-07-25 11:27:03 +0000

    added new line

--- wc.py
+++ wc.py
@@ -2,14 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 def wc():
-        word = raw_input("enter\n")
-        url= 'https://www.youtube.com/results?search_query=' +word
-        source_code=requests.get(url)
-        plain_text=source_code.text
-        soup=BeautifulSoup(plain_text,"html5lib")
+        word = raw_input("Enter your search \n")
+        url = 'https://www.youtube.com/results?search_query=' +word
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text,"html5lib")
         for link in soup.findAll('a',{'class':' yt-uix-sessionlink spf-link '}):
             href="https://youtube.com" +link.get('href')
             title=link.string
             print(href)
             print(title)
+            print("\n")
+            print('===========================================')
 wc()
